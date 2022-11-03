16658facbbe802cca6e026343cd6659a737e72ee
Vladimir <veliseykin2000@gmail.com>
2022-11-02 21:21:10 -0700

    Update main.py

--- main.py
+++ main.py
@@ -36,6 +36,7 @@ for image in html_doc_wikimedia.find_all('li', class_ = 'gallerybox'):
 
 
 #  description of images
+#  Doesn't work properly
 img_url = 'https://commons.wikimedia.org/wiki/File:'
 end_points = ['4-4-0 Inyo.jpg', 'AKC 2014 pics7 019.jpg', 'Dual_Coupling_Link_%26_Pin_with_Knuckle_Coupler.jpg', 'Engine_22,_2.JPG', 'Engine_22,_3.JPG', 'Engine 22.JPG', 'Locomotive 27.jpg', 'McKeen_Motor_Car_-22_Restoration.jpg', 'Nevada_State_Museum_at_Carson_City_NV_-_panoramio.jpg', 
 'Nevada_State_Museum_Inside_-_panoramio.jpg', 'Nevada_State_Railroad_Museum_-_panoramio_(1).jpg', 'Nevada_State_Railroad_Museum_-_panoramio_(12).jpg',
@@ -54,14 +55,13 @@ for end_point in end_points:
         for image in images_div.find('div', class_ = 'description mw-content-ltr en'):
             description_part1 = image.text.strip()
             description_list.append(description_part1)
-            print(description_part1)
+            
     except:
         images_description_2part = soup_images.find('td', class_ = 'description')
         description_part2 = images_description_2part.text.strip()
         description_list.append(description_part2)
-        print(description_part2)
-
-
+        
+   
 # Licens of images
     try:
         r_images_licens = requests.get(images_url)
