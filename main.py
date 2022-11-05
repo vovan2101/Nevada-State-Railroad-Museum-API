c98bc7fbad4bed4c38c74532ad31a2a666fc5e9f
Vladimir <veliseykin2000@gmail.com>
2022-11-05 16:37:59 -0700

    upgrade the program

--- main.py
+++ main.py
@@ -1,104 +1,107 @@
-from flask import Flask
-from flask import jsonify
 import requests
-import json
 from bs4 import BeautifulSoup
+import json
 
 
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-    return jsonify(all_info)
-
-
-# Taking first paragraph from wikiPEDIA
-url_wikipedia = 'https://en.wikipedia.org/wiki/Nevada_State_Railroad_Museum'
-
-r_wikipedia = requests.get(url_wikipedia)
-soup_wikipedia = BeautifulSoup(r_wikipedia.text, 'lxml')
-paragraph = soup_wikipedia.find('p').text
+# User put info about event that they are looking for
+state = str(input('Please enter state: '))
+print()
+city = str(input('Please enter city name: '))
+print()
+event_name = str(input('Please enter an event name: '))
+print()
+search = f'https://nominatim.openstreetmap.org/ui/search.html?q={state}+{city}+{event_name}'
 
 
-# Taking all images links from wikiMEDIA
-url_wikimedia = "https://commons.wikimedia.org/wiki/Category:Nevada_State_Railroad_Museum"
-
-r_wikimedia = requests.get(url_wikimedia)
-soup_wikimedia = BeautifulSoup(r_wikimedia.text, 'html.parser')
-html_doc_wikimedia = soup_wikimedia.find('ul', class_ ='gallery mw-gallery-traditional')
-
-list_images = []
-for image in html_doc_wikimedia.find_all('li', class_ = 'gallerybox'):
-    rows = image.find('div')
-    for link in rows.find('a'):
-        images_links = link.get('src')
-        list_images.append(images_links)
+# Taking osm id of event, to get more data about event
+search_json = requests.get(f'https://nominatim.openstreetmap.org/search.php?q={state}+{city}+{event_name}&format=jsonv2')
+osm_id_data = json.loads(search_json.text)
+osm_id = osm_id_data[0]['osm_id']
 
 
-#  description of images
-#  Doesn't work properly
-img_url = 'https://commons.wikimedia.org/wiki/File:'
-end_points = ['4-4-0 Inyo.jpg', 'AKC 2014 pics7 019.jpg', 'Dual_Coupling_Link_%26_Pin_with_Knuckle_Coupler.jpg', 'Engine_22,_2.JPG', 'Engine_22,_3.JPG', 'Engine 22.JPG', 'Locomotive 27.jpg', 'McKeen_Motor_Car_-22_Restoration.jpg', 'Nevada_State_Museum_at_Carson_City_NV_-_panoramio.jpg', 
-'Nevada_State_Museum_Inside_-_panoramio.jpg', 'Nevada_State_Railroad_Museum_-_panoramio_(1).jpg', 'Nevada_State_Railroad_Museum_-_panoramio_(12).jpg',
-'Nevada_State_Railroad_Museum_-_panoramio_(2).jpg', 'Nevada_State_Railroad_Museum_-_panoramio_(3).jpg', 'Nevada_State_Railroad_Museum_-_panoramio_(4).jpg', 'Nevada_State_Railroad_Museum_-_panoramio_(5).jpg', 'Nevada_State_Railroad_Museum_-_panoramio_(6).jpg', 'Nevada_State_Railroad_Museum_-_panoramio_(7).jpg', 
-'Nevada_State_Railroad_Museum_-_panoramio_(8).jpg', 'Nevada_State_Railroad_Museum_-_panoramio_(9).jpg', 'Nevada_State_Railroad_Museum_-_panoramio.jpg', 'NSRRMCC031.jpg', 'Velocipede_at_NRM.jpg']
+# All data from json page
+advanced_search_json = requests.get(f'https://nominatim.openstreetmap.org/details.php?osmtype=W&osmid={osm_id}&format=json')
+data = json.loads(advanced_search_json.text)
 
-for end_point in end_points:
-    images_url = img_url + end_point
-
-    r_images = requests.get(images_url)
-    soup_images = BeautifulSoup(r_images.text, 'lxml')
-
-    description_list = []
-    try:
-        images_div = soup_images.find('td', class_ = 'description')
-        for image in images_div.find('div', class_ = 'description mw-content-ltr en'):
-            description_part1 = image.text.strip()
-            description_list.append(description_part1)
-            
-    except:
-        images_description_2part = soup_images.find('td', class_ = 'description')
-        description_part2 = images_description_2part.text.strip()
-        description_list.append(description_part2)
-        
-   
-# Licens of images
-    try:
-        r_images_licens = requests.get(images_url)
-        soup_licens = BeautifulSoup(r_images_licens.text, 'lxml')  
-        images_lincens = soup_licens.find(class_ = 'rlicense-declaration').find('a', class_ = 'extiw').text
-    except:
-        pass
-
-
-# Taking all information about event from API
-response_museum = requests.get('https://nominatim.openstreetmap.org/details.php?osmtype=W&osmid=407063554&class=tourism&addressdetails=1&hierarchy=0&group_hierarchy=1&format=json')
-
-data = json.loads(response_museum.text)
-
+place_id = data['place_id']
+category = data['category']
 experience_name = data['names']['name']
 address_1 = data['addresstags']['street']
 address_2 = data['addresstags']['housenumber']
 city = data['addresstags']['city']
 state = data['addresstags']['state']
 zip = data['addresstags']['postcode']
+wikipedia_name = data['calculated_wikipedia'][2:]
 
 
-# Put all information I got, into the dict
+# Taking first paragraph from wikipedia if more then 50 elements, else taking two paragraphs
+wikipedia = f'https://en.wikipedia.org/wiki/{wikipedia_name}'
+wikipedia_request = requests.get(wikipedia)
+soup_wikipedia = BeautifulSoup(wikipedia_request.text, 'lxml')
+paragraph1 = soup_wikipedia.find('p').text.strip()
+
+if len(paragraph1) < 50:
+    paragraph2 = soup_wikipedia.find('p').find_next('p').text.strip()
+try:
+    experience_description = f'{paragraph1}\n\n{paragraph2}'
+except:
+    experience_description = paragraph1
+
+
+# All event images links
+images_url = requests.get(f'https://commons.wikimedia.org/wiki/Category{wikipedia_name}')
+soup_images = BeautifulSoup(images_url.text, 'lxml')
+images_wikimedia = soup_images.find('ul', class_ ='gallery mw-gallery-traditional')
+
+for image in images_wikimedia.find_all('li', class_ = 'gallerybox'):
+    images_title = image.find('a').get('href')
+    images_links = f'https://en.wikipedia.org/{images_title}'
+
+
+# Images description
+    images_description_url = requests.get(f'https://commons.wikimedia.org{images_title}')
+    soup_description = BeautifulSoup(images_description_url.text, 'lxml')
+    try:
+        description_wikimedia = soup_description.find('tbody').find('td', class_ = 'description').find('div', class_ = 'description mw-content-ltr en' )
+        for description in description_wikimedia:
+            description_images = description.text.strip()
+    except Exception:
+        description_wikimedia = soup_description.find('tbody').find('td', class_ = 'description')
+        for description in description_wikimedia:
+            description_images = description.text.strip()
+
+
+# Images license
+    soup_license = BeautifulSoup(images_description_url.text, 'lxml')
+    try:
+        license_wikimedia = soup_license.find('div', class_ = 'rlicense-declaration')
+        for license in license_wikimedia.find('a'):
+            images_license = license
+    except Exception:
+        license_wikimedia = soup_license.find('tbody')
+        for license in license_wikimedia.find('a'):
+            images_license = license
+
+
+# All images information together
+    images_info_list = []
+    images_info = f'{images_links} - {description_images} - {images_license}'
+    images_info_list.append(images_info)
+
+
+# All data about event
 all_info = {
+    'place_id' : place_id,
+    'category' : category,
     'experience_name': experience_name,
     'city' : city,
     'state' : state,
     'zip': zip,
     'address1': address_1,
     'address2' : address_2,
-    'wikipedia' : url_wikipedia,
-    'experience_description' : paragraph,
-    'images' : {
-    'images_licens' : images_lincens,
-    'images_description' : description_list,
-    'experience_images' : list_images,
-    }
+    'wikipedia_name' : wikipedia,
+    'experience_description' : experience_description,
+    'all_images_info' : images_info_list,
 }
 
 with open('all_info_json', 'w') as file:
