3db0ddc6c1ebbc19d6b4264e74b5ae71926e5aba
Vladimir <veliseykin2000@gmail.com>
2022-11-06 16:31:16 -0800

    .

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from doctest import Example
 import requests
 from bs4 import BeautifulSoup
 import json
@@ -83,11 +84,18 @@ for image in images_wikimedia.find_all('li', class_ = 'gallerybox'):
     try:
         description_wikimedia = soup_description.find('tbody').find('td', class_ = 'description').find('div', class_ = 'description mw-content-ltr en')
         for description in description_wikimedia:
-            description_images = description.text.strip()
+                description_images = description.text.strip()
+    except TypeError:
+        print(None)
+        continue
+    except AttributeError:
+        print(None)
+        continue
     except Exception:
         description_wikimedia = soup_description.find('tbody').find('td', class_ = 'description')
         for description in description_wikimedia:
             description_images = description.text.strip()
+    print(description_images)
 
 
 # Images license
@@ -107,26 +115,25 @@ for image in images_wikimedia.find_all('li', class_ = 'gallerybox'):
     images_info = f'{images_links} - {description_images} - {images_license}'
     images_info_list.append(images_info)
 
- 
+
 # All data about event
-all_info = {
-    'place_id' : place_id,
-    'category' : category,
-    'experience_name': experience_name,
-    'city' : city,
-    'state' : state,
-    'zip': zip,
-    'address1': address_1,
-    'address2' : address_2,
-    'wikipedia_name' : wikipedia,
-    'experience_description' : experience_description,
-    'all_images_info' : images_info_list,
-    'website' : website,
-    'phone_number' : phone_number,
-    'twitter' : twitter,
-}
+    all_info = {
+        'place_id' : place_id,
+        'category' : category,
+        'experience_name': experience_name,
+        'city' : city,
+        'state' : state,
+        'zip': zip,
+        'address1': address_1,
+        'address2' : address_2,
+        'wikipedia_name' : wikipedia,
+        'experience_description' : experience_description,
+        'website' : website,
+        'phone_number' : phone_number,
+        'twitter' : twitter,
+        'all_images_info' : images_info_list,
+    }
 
 with open('all_info_json', 'w') as file:
     json.dump(all_info, file, indent=4, ensure_ascii=False)
 
-print(all_info)
